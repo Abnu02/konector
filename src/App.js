2bcd10f0742a2f021af3a848afcbe957bf440816
Abreham Nuradis <abrehamnuradis222@gmail.com>
2024-12-06 20:04:31 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Footer from "./Componets/Footer";
+import Fourth from "./Componets/Fourth";
+import First from "./Componets/Frist";
+import Navbar from "./Componets/NavBar";
+import Second from "./Componets/Second";
+import Third from "./Componets/Third";
+import Fifth from "./Componets/Fifth";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <First />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
